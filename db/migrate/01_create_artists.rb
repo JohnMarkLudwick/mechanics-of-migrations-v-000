c1b55741ffc2907dd595ef0a0acc4ad7dbf088ab
JohnMark <johnmarkludwick@gmail.com>
2018-08-02 21:19:33 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,16 +1,20 @@
 class CreateArtists < ActiveRecord::Migration
+<<<<<<< HEAD
   def up
   end
  
   def down
   end
   
+=======
+>>>>>>> 06bdcccb1a624dc7b349e1e3e868d0b601088581
   def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
       t.string :hometown
+<<<<<<< HEAD
       end
     end
   end
@@ -18,3 +22,8 @@ class CreateArtists < ActiveRecord::Migration
   
   
   
+=======
+    end
+  end
+end
+>>>>>>> 06bdcccb1a624dc7b349e1e3e868d0b601088581
